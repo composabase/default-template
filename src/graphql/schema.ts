00481e1af5d6c4ce9194bdc95264b00d1b8f76df
composabot <composabot@composabase.com>
2025-01-07 22:59:10 +0000

    👷 fix: remove yarn cache

---
+++ src/graphql/schema.ts
@@ -0,0 +1,18 @@
+import { schema } from "@composabase/sdk"
+
+import helloCustom from '@composabase/graphql/modules/hello-custom'
+
+schema.query('hello', {
+  definition: {
+    type: schema.string(),
+    args: {
+      name: schema.string().optional(),
+      isImportant: schema.boolean().optional(),
+    },
+  },
+  resolver: 'hello',
+})
+
+schema.modules([helloCustom])
+
+export default schema
